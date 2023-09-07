fcfa521bac5bcb3c92c33609bc6823ac86acd887
Fred Amisi <fredyamisi@gmail.com>
2023-09-07 21:25:50 +0300

    i removed the links

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import Header from './Components/Header';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import Home from './Components/Home';
-import Services from './Components/Services';
+
 
 const App = () => {
   return (
     <>
-    <BrowserRouter>
     <Header />
-    <Routes>
-      <Route path="/Home" element={<Home />} />
-      <Route path="/Services" element={<Services />} />
-      
-    </Routes>
-    </BrowserRouter>
     </>
     
   );
